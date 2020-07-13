95b925c54d5a80a4ef19262717a04f46ba61fe56
Jonathan-124 <57970041+Jonathan-124@users.noreply.github.com>
2020-07-13 18:43:16 +0200

    Update urls.py

--- games/urls.py
+++ games/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import GameView, MethodologyView
+from .views import GameView, MethodologyView, SitemapView
 
 
 urlpatterns = [
     path('', GameView.as_view(), {'pk': 1}, name='game_view'),
     path('methodology/', MethodologyView.as_view(), {'pk': 1}, name='methodology_view'),
+    path('sitemap.xml/', SitemapView.as_view(), name='sitemap_view'),
 ]
